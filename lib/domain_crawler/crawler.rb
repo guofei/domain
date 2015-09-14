4375d84869cb0f2b93d69195f5bb45cc4dd4e440
guofei <guofei1986@gmail.com>
2015-09-14 16:59:58 +0900

    change thread

--- lib/domain_crawler/crawler.rb
+++ lib/domain_crawler/crawler.rb
@@ -10,9 +10,19 @@ module DomainCrawler
 
     def craw
       download = Downloader.new
-      lam = lambda{ history.pop || Parallel::Stop }
-      loop do
-        Parallel.each(lam, in_threads: 5) do |url|
+      # lam = lambda{ history.pop || Parallel::Stop }
+      n_thread = 5
+      Parallel.each(n_thread.times, in_threads: n_thread) do |_|
+        count = 0
+        loop do
+          url = history.pop
+          if url.nil?
+            sleep 10
+            break if count == 10
+            count += 1
+            next
+          end
+          count = 0
           download.links(url).each do |uri|
             pushed = history.push uri
             next unless pushed
@@ -22,9 +32,10 @@ module DomainCrawler
               next
             end
           end
+          # break if history.empty?
         end
-        break if history.empty?
       end
+      p 'end thread!!!'
     end
 
     private
